65b90bdee5468f7b6221e6182c777b72d91f27f6
MatthewLeaman <leaman.30@osu.edu>
2024-03-12 18:09:38 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App () {
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [image, setImage] = useState("");
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setImage(data);
+            setIsLoaded(true);
+        });
+    }, []);
+
+    if (!isLoaded) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <img alt="A Random Dog" src={image} ></img>
+    )
+}
+
+export default App;
