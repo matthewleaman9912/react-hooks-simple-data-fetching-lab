9e6e777260deee8248c6729b33a2ef79cbb9ce90
MatthewLeaman <leaman.30@osu.edu>
2024-03-12 18:15:39 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ function App () {
         fetch("https://dog.ceo/api/breeds/image/random")
         .then((response) => response.json())
         .then((data) => {
-            setImage(data);
+            setImage(data.message);
             setIsLoaded(true);
         });
     }, []);
